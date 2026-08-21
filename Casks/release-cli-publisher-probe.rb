478860d86e2c515222c4862f7ebdb5dbba004282
Joshua Gilman <joshuagilman@gmail.com>
2026-08-20 17:37:03 -0700

    chore(cask): update release-cli-publisher-probe to 0.1.3

---
+++ Casks/release-cli-publisher-probe.rb
@@ -0,0 +1,44 @@
+# Generated from the published v0.1.3 release cask for publisher reconciliation testing.
+cask "release-cli-publisher-probe" do
+  version "0.1.3"
+
+  on_macos do
+    on_arm do
+      sha256 "f9ac64eb1b1191d58176216c4e9ccffe0cfce0ea39f219e9c65a429dcf5b2aaa"
+      url "https://github.com/meigma/release/releases/download/v#{version}/release-cli_#{version}_darwin_arm64.tar.gz"
+    end
+    on_intel do
+      sha256 "5709c1fbf62ee121dfdf91d92a3c7ce5c4aaa566ee15a530067c6f161e7fc7a6"
+      url "https://github.com/meigma/release/releases/download/v#{version}/release-cli_#{version}_darwin_amd64.tar.gz"
+    end
+  end
+  on_linux do
+    on_arm do
+      sha256 "f45226f291519ae4e34fec9ab5ac0e4501225aad92ee3bdaa1f5d2f5c889059b"
+      url "https://github.com/meigma/release/releases/download/v#{version}/release-cli_#{version}_linux_arm64.tar.gz"
+    end
+    on_intel do
+      sha256 "8d6cd431d18dc9e52f845bb096947f666e10a568f41873aa7e463e0e83920d0b"
+      url "https://github.com/meigma/release/releases/download/v#{version}/release-cli_#{version}_linux_amd64.tar.gz"
+    end
+  end
+
+  name "release-cli publisher probe"
+  desc "Live reconciliation probe for the Meigma release CLI"
+  homepage "https://github.com/meigma/release"
+
+  livecheck do
+    skip "Generated from a released artifact."
+  end
+
+  binary "release-cli"
+
+  postflight do
+    if OS.mac?
+      system_command "/usr/bin/xattr",
+                     args: ["-dr", "com.apple.quarantine", "#{staged_path}/release-cli"]
+    end
+  end
+
+  # No zap stanza required
+end
